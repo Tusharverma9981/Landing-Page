850c4928625387a13a29b667a9d2123538d1e36b
Tushar <tv4648551@gmail.com>
2025-04-17 23:41:33 +0530

    made the page 2

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function loader() {
     tl.to("#loader",{
         display:"none"
     })
-    tl.from(".hero h1",{
+    tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
         y:120,
         stagger:0.2
     })
